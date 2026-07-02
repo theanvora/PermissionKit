31a3c4df7159f266c558f16e086593f6a01b1a39
imx <phtuanh.imx@gmail.com>
2026-07-02 21:29:46 +0700

    chore: right-size minimum deployment target to the lowest API actually used

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "PermissionKit",
     platforms: [
-        .iOS("26.0")
+        .iOS("18.0")
     ],
     products: [
         .library(name: "PermissionCore",         targets: ["PermissionCore"]),
